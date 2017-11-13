1311aae489e0be946bcc8342a8fa9255c418d5ef
Hyun Seung Jeong <ahribori@gmail.com>
2017-11-13 14:45:36 +0900

    - #25484: 로그인 컴포넌트 개발

--- packages/polestar-template/examples/login/index.js
+++ packages/polestar-template/examples/login/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'styles/style.css';
+import Login from './components/Login';
+import './styles/style.css';
+import Logo from './styles/images/polestarlogo.png';
 
 ReactDOM.render(
-    <div>로그인 화면</div>,
-    document.getElementById('app')
+    <div>
+        <Login
+            width={500}
+
+            usernameName="username"
+            usernameId="usernameId"
+            usernameClassName="polestar login username"
+            usernameLableText="사용자 계정"
+            usernamePlaceholder="사용자 계정을 입력하세요"
+            usernamePattern="[A-Za-z0-9]{4,16}"
+
+            passwordName="password"
+            passwordId="passwordId"
+            passwordClassName="polestar login password"
+            passwordLableText="패스워드"
+            passwordPlaceholder="패스워드를 입력하세요"
+            passwordPattern="[A-Za-z0-9]{4,16}"
+
+            loginButtonClassName="polestar login button"
+            loginButton={(<button>로그인!</button>)}
+
+            logo={(<img src={Logo} alt="logo" className="polestar login logo" />)}
+
+            containerClassName="polestar login container"
+
+            onChange={(field) => {
+                console.log('onChange -> ', field);
+            }}
+
+            onSubmit={(fields) => {
+                console.log('onSubmit ->', fields);
+            }}
+
+            loginProcess={(fields) => {
+                console.log('loginProcess ->', fields);
+                window.location.href = 'http://nis.nkia.net';
+            }}
+        />
+    </div>,
+    document.getElementById('app'),
 );

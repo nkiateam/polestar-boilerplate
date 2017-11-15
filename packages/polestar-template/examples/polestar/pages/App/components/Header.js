e762606fe64755b104046214ec0f3633687af06e
Hyun Seung Jeong <ahribori@gmail.com>
2017-11-15 10:16:46 +0900

    - 해상도가 작아졌을 때, topMenu -> 햄버거 버튼으로 숨김

--- packages/polestar-template/examples/polestar/pages/App/components/Header.js
+++ packages/polestar-template/examples/polestar/pages/App/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Menu, Button, Row, Col } from 'antd';
+import { Layout, Menu, Button, Row, Col, Dropdown } from 'antd';
 
 class Header extends React.Component {
     static propTypes = {};
@@ -21,6 +21,22 @@ class Header extends React.Component {
     );
 
     /**
+     * 메뉴 목록을 렌더링 하는 함수
+     * @param direction {'horizontal' || 'vertical'}
+     */
+    renderMenu = (direction = 'vertical') => (
+        <Menu theme="dark" mode={direction} defaultSelectedKeys={['1']}>
+            <Menu.Item key="0">리소스관리</Menu.Item>
+            <Menu.Item key="1">대시보드</Menu.Item>
+            <Menu.Item key="2">토폴로지맵</Menu.Item>
+            <Menu.Item key="3">성능통계</Menu.Item>
+            <Menu.Item key="4">알람콘솔</Menu.Item>
+            <Menu.Item key="5">이벤트뷰어</Menu.Item>
+            <Menu.Item key="6">시스템관리</Menu.Item>
+        </Menu>
+    );
+
+    /**
      * 헤더 영역의 대메뉴 목록을 렌더링하는 함수 (리소스관리, 대시보드, 토폴로지맵 ...)
      */
     renderTopMenu = () => (
@@ -32,16 +48,18 @@ class Header extends React.Component {
             className="menu-bars"
             style={{ textAlign: 'center', padding: '1px 0px 1px 0px' }}
         >
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                <Menu.Item key="0">리소스관리</Menu.Item>
-                <Menu.Item key="1">대시보드</Menu.Item>
-                <Menu.Item key="2">토폴로지맵</Menu.Item>
-                <Menu.Item key="3">성능통계</Menu.Item>
-                <Menu.Item key="4">알람콘솔</Menu.Item>
-                <Menu.Item key="5">이벤트뷰어</Menu.Item>
-                <Menu.Item key="6">시스템관리</Menu.Item>
-            </Menu>
+            {this.renderMenu('horizontal')}
         </Col>
+    );
+
+    /**
+     * 화면 폭이 좁아지면 상단 대메뉴가 우측 상단 햄버거 메뉴로 숨겨지게 되는데,
+     * 햄버거 버튼에 마우스오버했을 때 나오는 드롭다운을 렌더링하는 함수
+     */
+    renderDropdownMenu = () => (
+        <Dropdown overlay={this.renderMenu('vertical')}>
+            <Button shape="circle" icon="menu-fold" className="menu-icon" style={{ marginRight: '6px' }} />
+        </Dropdown>
     );
 
     /**
@@ -49,7 +67,7 @@ class Header extends React.Component {
      */
     renderMenuButtons = () => (
         <Col xs={16} sm={16} md={4} lg={3} style={{ textAlign: 'right' }}>
-            <Button shape="circle" icon="menu-fold" className="menu-icon" style={{ marginRight: '6px' }} />
+            {this.renderDropdownMenu()}
             <Button shape="circle" icon="search" style={{ marginRight: '6px' }} />
             <Button shape="circle" icon="bell" style={{ marginRight: '6px' }} />
             <Button shape="circle" icon="user" style={{ marginRight: '6px' }} />

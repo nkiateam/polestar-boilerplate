3883168a034330a4027057b20c8178dba2e894de
elle510 <elle0510@gmail.com>
2018-05-02 10:28:44 +0900

    add eslint rule

--- packages/polestar-template/config/.eslintrc.js
+++ packages/polestar-template/config/.eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
   "rules": {
     "max-len": [1, 120, 2, {"ignoreComments": true}],
     "indent": [1, 4, { "SwitchCase": 1 }],
-    "arrow-body-style": ["error", "as-needed", { "requireReturnForObjectLiteral": true }],
+    "arrow-body-style": "off", // ["error", "as-needed", { "requireReturnForObjectLiteral": true }],
+	  "arrow-parens": "off", // Incompatible with prettier
+	  //"no-useless-constructor": "off",
+	  //"no-console": "error", // airbnb is using warn
     "react/jsx-indent": [1, 4],
     "linebreak-style": 'off', // Don't play nicely with Windows.
     "react/jsx-filename-extension": ["error", { extensions: [".js"] }], // airbnb is using .jsx
@@ -11,11 +14,15 @@ module.exports = {
     "react/prop-types": "off",
     "react/jsx-indent-props": [2, 4],
     "react/forbid-prop-types": "off",
+	  "react/require-default-props": "off", // airbnb use error
     "react/default-props-match-prop-types": "off",
     "jsx-a11y/href-no-hash": "off",
     "import/no-extraneous-dependencies": "off",
     "import/no-unresolved": "off",
-    "import/extensions": "off"
+    "import/extensions": "off",
+    "no-underscore-dangle": "off",
+    "no-param-reassign": ["error", { "props": false }],
+    "no-plusplus": "off"
   },
   "settings" : {
 	  "import/resolver": {
